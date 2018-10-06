89b9dc9e8face2017019d54745f1bcb35d8c7a7c
Peter M C Harrison <pmc.harrison@gmail.com>
2018-10-06 11:57:23 +0100

    Added README and NEWS

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,5 +1,35 @@
 context("test-main")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Prefer small numbers", {
+  x <- lapply(1:10, function(x) 1:10)
+  cost_funs <- list(cost_fun(FALSE, identity))
+  res <- seq_opt(x, cost_funs)
+  expect_equal(unlist(res), rep(1, times = 10))
+})
+
+test_that("Prefer large numbers", {
+  x <- lapply(1:10, function(x) 1:10)
+  cost_funs <- list(cost_fun(FALSE, `-`))
+  res <- seq_opt(x, cost_funs)
+  expect_equal(unlist(res), rep(10, times = 10))
+})
+
+test_that("Must increase, prefer small changes", {
+  x <- lapply(1:10, function(x) 1:10)
+  cost_funs <- list(
+    cost_fun(TRUE, function(a, b) if (b <= a) Inf else 0),
+    cost_fun(TRUE, function(a, b) abs(b - a))
+  )
+  res <- unlist(seq_opt(x, cost_funs))
+  expect_equal(res, 1:10)
+})
+
+test_that("Changing available states", {
+  x <- lapply(1:10, function(x) 10 * x + 0:9)
+  cost_funs <- list(
+    # Difference between successive states should be divisible by 9
+    cost_fun(TRUE, function(a, b) abs(((b - a) %% 9) - 0))
+  )
+  res <- unlist(seq_opt(x, cost_funs))
+  expect_equal(res, c(10, 28, 37, 46, 55, 64, 73, 82, 91, 100))
 })
