597abc1879fe124b299c02ea47213c219910ed21
Peter M C Harrison <pmc.harrison@gmail.com>
2019-02-10 12:04:56 +0000

    summarise_problem updated number formatting

--- R/summarise-problem.R
+++ R/summarise-problem.R
@@ -1,14 +1,17 @@
 summarise_problem <- function(x) {
   N <- length(x)
-  message("Optimising a sequence of length ", N, ".")
+  message("Optimising a sequence of length ",
+          N %>% format(scientific = FALSE, big.mark = ","),
+          ".")
   num_alts <- purrr::map_int(x, length)
   message("Each position has an average of ",
-          mean(num_alts) %>% round(2),
+          mean(num_alts) %>% round(2) %>% format(scientific = FALSE, big.mark = ","),
           " alternatives.")
   num_transitions <- sum(purrr::map2_int(
     num_alts[- N], num_alts[- 1],
     function(context_n, next_n) context_n * next_n))
-  message("The algorithm needs to calculate ", num_transitions,
+  message("The algorithm needs to calculate ",
+          num_transitions %>% format(scientific = FALSE, big.mark = ","),
           " transition costs.")
   invisible(list(N = N, num_alts = num_alts, num_transitions = num_transitions))
 }
